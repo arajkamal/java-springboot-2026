9a8ce8b91aba314ea3cfbdcae59248f9d9f30ebe
rajkamal <Arajkamal93@gmail.com>
2026-02-10 01:49:34 +0530

    Create leetcode141LinkedListCycle.java

---
+++ dsa-problems/src/linkedList/leetcode141LinkedListCycle.java
@@ -0,0 +1,34 @@
+package src.linkedList;
+
+public class leetcode141LinkedListCycle {
+    public static void main(String[] args) {
+        System.out.println("rajkamal");
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
